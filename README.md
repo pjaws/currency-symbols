cf2f8aa4790a806ac7e5f2484a4d25800d8c7994
Paul Jaworski <paulmattjaws@gmail.com>
2019-11-01 12:24:48 -0700

    initial commit - support for all unicode currency symbols

---
+++ README.md
@@ -0,0 +1,38 @@
+# CurrencySymbols.js
+
+A tiny library that maps currency codes to currency symbols. No dependencies. Several ways to get the symbol you want.
+
+Currently supports all currency symbols with a Unicode value. Support for other currencies coming soon!
+
+## Installation
+
+`npm install currency-symbols`
+
+## Usage
+
+**Use the helper function**
+
+```
+import getCurrencySymbol from 'currency-symbols';
+
+getCurrencySymbol('USD'); // '$'
+getCurrencySymbol('EUR'); // '€'
+getCurrencySymbol('INVALID') // undefined
+```
+
+**Import a constant matching the symbol you need**
+
+```
+import { USD, EUR } from 'currency-symbols';
+
+console.log(`American Dollar: ${USD}`); // American Dollar: $
+console.log(`Euro: ${EUR}`); // Euro: €
+```
+
+**Import the whole damn map**
+
+```
+import { CURRENCY_SYMBOLS } from 'currency-symbols';
+
+console.log(`British Pound: ${CURRENCY_SYMBOLS.GBP}`);
+```
---
+++ index.js
@@ -0,0 +1,69 @@
+export const AUD = '$'; // Australian Dollar
+export const BDT = '৳'; // Bangladeshi Taka
+export const BTC = '₿'; // Bitcoin
+export const CAD = '$'; // Canadian Dollar
+export const CNY = '¥'; // Chinese Yuan
+export const CRC = '₡'; // Costa Rican Colón
+export const ETH = 'Ξ'; // Ethereum
+export const EUR = '€'; // Euro
+export const GBP = '£'; // British Pound Sterling
+export const GEL = '₾'; // Georgian Lari
+export const HKD = '$'; // Hong Kong Dollar
+export const ILS = '₪'; // Israeli New Sheqel
+export const INR = '₹'; // Indian Rupee
+export const JPY = '¥'; // Japanese Yen
+export const KRW = '₩'; // South Korean Won
+export const LTC = 'Ł'; // Litecoin
+export const MXN = '$'; // Mexican Peso
+export const NGN = '₦'; // Nigerian Naira
+export const NZD = '$'; // New Zealand Dollar
+export const PHP = '₱'; // Philippine Peso
+export const PLN = 'zł'; // Polish Zloty
+export const PYG = '₲'; // Paraguayan Guarani
+export const RUB = '₽'; // Russian Ruble
+export const SGD = '$'; // Singapore Dollar
+export const THB = '฿'; // Thai Baht
+export const TRY = '₺'; // Turkish Lira
+export const UAH = '₴'; // Ukrainian Hryvna
+export const USD = '$'; // US Dollar
+export const VND = '₫'; // Vietnamese Dong
+export const XMR = 'ɱ'; // Monero
+
+const CURRENCY_SYMBOLS = {
+	AUD,
+	BDT,
+	BTC,
+	CAD,
+	CNY,
+	CRC,
+	ETH,
+	EUR,
+	GBP,
+	GEL,
+	HKD,
+	ILS,
+	INR,
+	JPY,
+	KRW,
+	LTC,
+	MXN,
+	NGN,
+	NZD,
+	PHP,
+	PLN,
+	PYG,
+	RUB,
+	SGD,
+	THB,
+	TRY,
+	UAH,
+	USD,
+	VND,
+	XMR,
+};
+Object.freeze(CURRENCY_SYMBOLS);
+export CURRENCY_SYMBOLS;
+
+export default function getCurrencySymbol(symbol) {
+	return CURRENCY_SYMBOLS[symbol];
+}
